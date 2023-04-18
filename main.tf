babef2202082f8223f260aa2972dd6d4900b7255
judekaney <111720701+judekaney@users.noreply.github.com>
2023-04-17 20:31:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = " 4.62.0"
+    backend "remote" {
+        organization = "judekaney"
+        workspaces {
+            name = "resume-gitactions"
         }
     }
 }
