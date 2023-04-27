9aa0caa504936066af32a9ab1a78245976be3c4f
judekaney <111720701+judekaney@users.noreply.github.com>
2023-04-26 21:16:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -151,6 +151,9 @@ resource "aws_api_gateway_method_response" "methodresponse" {
   resource_id             = data.aws_api_gateway_resource.visitorget.id
   http_method             = aws_api_gateway_method.method.http_method
   status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
 }
 
 resource "aws_api_gateway_integration_response" "response" {
@@ -158,6 +161,9 @@ resource "aws_api_gateway_integration_response" "response" {
   resource_id             = data.aws_api_gateway_resource.visitorget.id
   http_method             = aws_api_gateway_method.method.http_method
   status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
 }
 
 module "cors" {
